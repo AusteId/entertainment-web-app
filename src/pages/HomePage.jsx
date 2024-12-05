3044269291792331fedd95898c8c0c511a47f348
AusteId <auste.idaite@gmail.com>
2024-12-06 00:33:13 +0200

    Small change

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,7 @@ import SignUpForm from "../components/SignUpForm"
 export default function HomePage() {
   return (
     <main>
-      {/* <h1 className="heading-lg">Home page cia</h1> */}
-      <SignUpForm/>
+      <h1 className="heading-lg">Home page cia</h1>
     </main>
   );
 }
