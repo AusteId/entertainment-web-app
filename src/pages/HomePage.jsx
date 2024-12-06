1611fb9a17ebc53c636dadf7bc3b50f5eb0f6f39
D2JS <d2js@TECHIN.INTERNAL>
2024-12-06 12:30:46 +0200

    Added BasicCard component and added it to the HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
+import { BasicCard } from "../components/shared/BasicCard";
+
 export default function HomePage() {
   return (
+    <>
     <main>
       <h1 className="heading-lg">Home page cia</h1>
+    
     </main>
+    <div>
+    <h2 className="heading-lg text-white font-outfit mb-8">Recommended for you</h2>
+    <div className="grid grid-cols-4 gap-10">
+    <BasicCard />
+    </div>
+    </div>
+    </>
   );
 }
