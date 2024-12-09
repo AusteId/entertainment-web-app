340618887e11f3c14df0f96ded2ad9fd1744e96e
AusteId <auste.idaite@gmail.com>
2024-12-09 13:35:27 +0200

    Initial SignUpForm and Button components setup

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -5,6 +5,7 @@ import { getByEmail } from "../api/get";
 import { addUser } from "../api/post";
 import { generateHash } from "../utils/passwordHash";
 import Button from "./shared/Button";
+import logo from "/assets/logo.svg";
 
 const SignUpForm = () => {
     const { register, handleSubmit, formState: { errors }, reset, setError } = useForm();
@@ -52,57 +53,63 @@ const SignUpForm = () => {
     }
 
     return (
-        < div className=" flex justify-center min-h-screen items-center">
-            <section className="grid border rounded-[1.25rem] bg-darkBlue w-[20.4375rem] min-h-[26.25rem] md:w-[25rem] md:min-h-[26.125rem]">
-                <h1 className="text-white font-medium text-hl font-outfit pt-[2rem] pl-[2rem] pb-[2.5rem]">Sign Up</h1>
-                <form onSubmit={handleSubmit(formSubmitHandler)} className="grid" noValidate>
-                    <div className="px-[2rem] pb-[1.5rem] relative">
-                        <input type="email" placeholder="Email address" className={`text-bm bg-darkBlue border-b ${errors.password ? "border-red" : "border-lightBlue"} w-full placeholder:font-outfit placeholder:font-light placeholder:text-bm h-[2.3125rem] pl-[1rem] placeholder:leading-[1.5] py-[0.25rem] pb-[1.12rem] caret-red text-white focus:outline-none focus:border-white`} {...register("email", {
-                            required: "Can't be empty",
-                            pattern: {
-                                value: /^(?=.{1,254}$)(?=.{1,64}@)[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
-                                message: "Invalid email address",
-                            },
-                        })}
-                        />
-                        <p className={`font-outfit text-bs text-red ${errors.email?.message.includes("empty") ? "absolute top-0 right-[3.06rem]" : "pt-1"} font-medium whitespace-nowrap`}>{errors.email?.message}</p>
+        <>
+            <div className="flex justify-center">
+                <img src={logo} alt="logo" className="pt-[3rem] md:pt-[5.5rem] xl:pt[4.9rem]" />
+            </div>
+            < div className=" flex justify-center min-h-screen items-center">
+                <section className="grid border rounded-[1.25rem] bg-darkBlue w-[20.4375rem] min-h-[26.25rem] md:w-[25rem] md:min-h-[26.125rem]">
+                    <h1 className="text-white font-medium text-hl font-outfit pt-[2rem] pl-[2rem] pb-[2.5rem]">Sign Up</h1>
+                    <form onSubmit={handleSubmit(formSubmitHandler)} className="flex flex-col" noValidate>
+                        <div className="px-[1.5rem] pb-[1.5rem] md:px-[2rem] relative">
+                            <input type="email" placeholder="Email address" className={`text-bm bg-darkBlue border-b ${errors.password ? "border-red" : "border-lightBlue"} placeholder:font-outfit placeholder:font-light placeholder:text-bm w-full md:w-[21rem]
+                        h-[2.3125rem] pl-[1rem] placeholder:leading-[1.5] py-[0.25rem] pb-[1.12rem] caret-red text-white focus:outline-none focus:border-white`} {...register("email", {
+                                required: "Can't be empty",
+                                pattern: {
+                                    value: /^(?=.{1,254}$)(?=.{1,64}@)[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+                                    message: "Invalid email address",
+                                },
+                            })}
+                            />
+                            <p className={`font-outfit text-bs text-red ${errors.email?.message.includes("empty") ? "absolute top-0 right-[3.06rem]" : "pt-1"} font-medium whitespace-nowrap`}>{errors.email?.message}</p>
+                        </div>
+                        <div className="px-[1.5rem] pb-[1.5rem] md:px-[2rem] relative">
+                            <input type="password" placeholder="Password" className={`text-bm bg-darkBlue border-b ${errors.password ? "border-red" : "border-lightBlue"} w-full placeholder:font-outfit placeholder:font-light placeholder:text-bm h-[2.3125rem] pl-[1rem] pb-[1.12rem] caret-red text-white focus:outline-none focus:border-white`} {...register("password", {
+                                required: "Can't be empty",
+                                minLength: {
+                                    value: 8,
+                                    message: "Password must be at least 8 characters long",
+                                },
+                                pattern: {
+                                    value: /^(?=.*[a-z])(?=.*[A-Z]).+$/,
+                                    message: "Password must contain both uppercase and lowercase letters",
+                                }
+                            })} />
+                            <p className={`font-outfit text-bs text-red ${errors.password?.message.includes("empty") ? "absolute top-0 right-[3.06rem]" : "pt-1 text-[0.9rem] text-wrap"} font-medium whitespace-nowrap`}>{errors.password?.message}</p>
+                        </div>
+                        <div className="px-[1.5rem] pb-[1.5rem] md:px-[2rem] relative">
+                            <input type="password" placeholder="Repeat password" className={`text-bm bg-darkBlue border-b ${errors.password ? "border-red" : "border-lightBlue"} w-full placeholder:font-outfit placeholder:font-light placeholder:text-bm h-[2.3125rem] pl-[1rem] pb-[1.12rem] caret-red text-white focus:outline-none focus:border-white`} {...register("repeatPassword", {
+                                required: "Can't be empty",
+                                minLength: {
+                                    value: 8,
+                                    message: "Password must be at least 8 characters long",
+                                },
+                                pattern: {
+                                    value: /^(?=.*[a-z])(?=.*[A-Z]).+$/,
+                                    message: "Password must contain both uppercase and lowercase letters",
+                                }
+                            })} />
+                            <p className={`font-outfit text-bs text-red ${errors.repeatPassword?.message.includes("empty") ? "absolute top-0 right-[3.06rem]" : "pt-1 text-[0.9rem] text-wrap"} font-medium whitespace-nowrap`}>{errors.repeatPassword?.message}</p></div>
+                        <div className="pb-[1.5rem] px-[1.5rem] text-center">
+                            <Button buttonText="Create an account" />
+                        </div>
+                    </form>
+                    <div className="inline-block text-center pb-[2rem]">
+                        <p className="text-white text-bm font-outfit font-medium">Already have an account?<span className="text-red pl-[0.5rem] font-outfit font-medium"><a href="#">Login</a></span></p>
                     </div>
-                    <div className="px-[2rem] pb-[1.5rem] relative">
-                        <input type="password" placeholder="Password" className={`text-bm bg-darkBlue border-b ${errors.password ? "border-red" : "border-lightBlue"} w-full placeholder:font-outfit placeholder:font-light placeholder:text-bm h-[2.3125rem] pl-[1rem] pb-[1.12rem] caret-red text-white focus:outline-none focus:border-white`} {...register("password", {
-                            required: "Can't be empty",
-                            minLength: {
-                                value: 8,
-                                message: "Password must be at least 8 characters long",
-                            },
-                            pattern: {
-                                value: /^(?=.*[a-z])(?=.*[A-Z]).+$/,
-                                message: "Password must contain both uppercase and lowercase letters",
-                            }
-                        })} />
-                        <p className={`font-outfit text-bs text-red ${errors.password?.message.includes("empty") ? "absolute top-0 right-[3.06rem]" : "pt-1 text-[0.9rem] text-wrap"} font-medium whitespace-nowrap`}>{errors.password?.message}</p>
-                    </div>
-                    <div className="px-[2rem] pb-[1.5rem] relative">
-                        <input type="password" placeholder="Repeat password" className={`text-bm bg-darkBlue border-b ${errors.password ? "border-red" : "border-lightBlue"} w-full placeholder:font-outfit placeholder:font-light placeholder:text-bm h-[2.3125rem] pl-[1rem] pb-[1.12rem] caret-red text-white focus:outline-none focus:border-white`} {...register("repeatPassword", {
-                            required: "Can't be empty",
-                            minLength: {
-                                value: 8,
-                                message: "Password must be at least 8 characters long",
-                            },
-                            pattern: {
-                                value: /^(?=.*[a-z])(?=.*[A-Z]).+$/,
-                                message: "Password must contain both uppercase and lowercase letters",
-                            }
-                        })} />
-                        <p className={`font-outfit text-bs text-red ${errors.repeatPassword?.message.includes("empty") ? "absolute top-0 right-[3.06rem]" : "pt-1 text-[0.9rem] text-wrap"} font-medium whitespace-nowrap`}>{errors.repeatPassword?.message}</p></div>
-                    <div className="pb-[1.5rem] text-center">
-                        <Button buttonText="Create an account" />
-                    </div>
-                </form>
-                <div className="inline-block text-center pb-[2rem]">
-                    <p className="text-white text-bm font-outfit font-medium">Already have an account?<span className="text-red pl-[0.5rem] font-outfit font-medium"><a href="#">Login</a></span></p>
-                </div>
-            </section>
-        </div>
+                </section>
+            </div>
+        </>
     );
 }
 
--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({buttonText}) => {
     return (
-        <button className="w-[17.4375rem] h-[3rem] md:w-[21rem] bg-red text-white text-bm font-outfit font-medium rounded-[0.375rem]">{buttonText}</button>
+        <button className="w-[17.4375rem] h-[3rem] md:w-[21rem] bg-red text-white text-bm font-outfit font-medium rounded-[0.375rem] hover:bg-white hover:text-dark">{buttonText}</button>
     )
 }
 
