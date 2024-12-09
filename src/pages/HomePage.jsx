288b7113cd4a4957654ba4dc16c92c4cea8db7f3
D1JP <d1jp@TECHIN.INTERNAL>
2024-12-09 08:39:44 +0200

    NavBar 0.9.1 - micro fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
+import NavBar from './../components/NavBar'
 export default function HomePage() {
   return (
+    <>
+<NavBar />
     <main>
       <h1 className="heading-lg">Home page cia</h1>
     </main>
+    </>
   );
 }
