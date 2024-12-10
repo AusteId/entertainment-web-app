31fc9e948bdbbcfc65bf773d59331753bcd72758
dncworking <dominyka.dobuzinskaite@gmail.com>
2024-12-10 10:11:37 +0200

    Revert "Improved Button component and other minor changes"

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ({ children, onClick, type }) => {
+const Button = ({buttonText}) => {
     return (
-        <button onClick={onClick} type={type} className="w-[17.4375rem] h-[3rem] md:w-[21rem] bg-red text-white text-bm font-outfit font-medium rounded-[0.375rem] hover:bg-white hover:text-dark">{children}</button>
+        <button className="w-[17.4375rem] h-[3rem] md:w-[21rem] bg-red text-white text-bm font-outfit font-medium rounded-[0.375rem] hover:bg-white hover:text-dark">{buttonText}</button>
     )
 }
 
