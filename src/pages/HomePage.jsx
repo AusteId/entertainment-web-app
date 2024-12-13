4708f66a88ea9901cab1d0656870fe6cca053cc6
marijustechin <marijus.smiginas@stud.techin.lt>
2024-12-13 08:53:47 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,49 @@
-import { TrendingCard } from "../components/shared/TrendingCard";
+import { useUserContext } from '../service/UserContextProvider';
+import { Search } from '../components/Search';
+import { apiGetHomeMovies } from '../api/movies';
+import { useEffect, useState } from 'react';
+import MoviesList from '../components/MoviesList';
 
 export default function HomePage() {
+  const userData = useUserContext();
+  const [searchText, setSearchText] = useState('');
+  const [trending, setTrending] = useState([]);
+  const [recommended, setRecommended] = useState([]);
+  const [filteredRecommended, setFilteredRecommended] = useState([]);
+
+  useEffect(() => {
+    if (userData.userId) getHomeMovies(userData.userId);
+  }, []);
+
+  const getHomeMovies = async (userId) => {
+    const homeMovies = await apiGetHomeMovies(userId);
+    if (!homeMovies.error) {
+      setTrending(homeMovies.trendingMovies);
+      setRecommended(homeMovies.recommendedMovies);
+      setFilteredRecommended(homeMovies.recommendedMovies);
+    }
+  };
+
+  const handleSearch = (textString) => {
+    if (textString) {
+      setFilteredRecommended([
+        ...recommended.filter((movie) =>
+          movie.title.toLowerCase().includes(textString.toLowerCase())
+        ),
+      ]);
+    } else {
+      setFilteredRecommended([...recommended]);
+    }
+
+    setSearchText(textString);
+  };
+
   return (
-    <main>
-      <h1 className="heading-lg">Home page cia</h1>
-      <TrendingCard/>
-    </main>
+    <div className="w-full flex flex-col gap-4 body-md p-4">
+      {/*       Vieta trending slideriui - kur jis???        */}
+      <Search onSearch={(searchString) => handleSearch(searchString)} />
+
+      <MoviesList movies={filteredRecommended} searchText={searchText} />
+    </div>
   );
 }
