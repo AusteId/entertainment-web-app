f72b0cc4fa288fbb75ff327dd50f8a4975e19079
TadasR <tadas.rumsas@gmail.com>
2024-12-18 21:34:14 +0200

    add play button small screens

--- src/components/shared/BasicCard.jsx
+++ src/components/shared/BasicCard.jsx
@@ -36,28 +36,19 @@ export const BasicCard = ({ Card }) => {
             <img
               src={Card.thumbnail.regular.large}
               alt="thumbnail"
-              className="rounded-md thumbnail-img lg:group-hover:brightness-50 duration-200"
+              className="rounded-md thumbnail-img group-hover:brightness-50 duration-200"
             />
           </picture>
-          <button
-            className="absolute flex items-center opacity-0 group-hover:opacity-100 rounded-full 
-            duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 desktop-only"
-          >
-            <svg
-              className="play-image"
-              width="30"
-              height="30"
-              viewBox="0 0 30 30"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 0C6.713 0 0 6.713 0 15c0 8.288 6.713 15 15 15 8.288 0
-                15-6.712 15-15 0-8.287-6.712-15-15-15Zm-3 21V8l9 6.5-9 6.5Z"
-                fill="#FFF"
-              />
-            </svg>
-            <p className="heading-xs text-white font-outfit play-text">Play</p>
-          </button>
+          
+          <button className="absolute flex items-center opacity-0 group-hover:opacity-100 rounded-full duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div className="ml-3 mr-4">
+                  <img src="/assets/icon-play.svg" alt="play" />
+                </div>
+                <p className="text-hm text-white font-medium font-outfit">
+                  Play
+                </p>
+              </button>
+          
         </div>
 
         <div className="list-text-color">
